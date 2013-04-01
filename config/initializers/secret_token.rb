3d9e002c37201dac3bab52ea6d7b4652f2936423
Annie Vang <avan2@amfam.com>
2013-04-01 09:08:01 -0500

    Initial commit by me

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yoyobird::Application.config.secret_token = 'e04db3debf09b2e3c2b399d711bc1b520e784e036385ee74bf598877ecfb3e98f14e1525e2cc7d6380653b0dd3b9eab9167842d415eab0ec9ba287968debc48b'
